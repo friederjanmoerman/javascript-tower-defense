4f17fa39800a927723bbe0cd8d742aa70f7e1612
friederjanmoerman <hijbrid@gmail.com>
2018-12-26 12:27:04 +0100

    path finding added

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,11 +94,71 @@ game.enemies =
     entity.speed = 1;
     entity.strength = strength;
     entity.velocity = {x:entity.speed, y:entity.speed};
+    entity.waypoint = false;
+    entity.waypointIndex = 0;
     entity.update = function()
     {
-      this.x += entity.velocity.x;
-      this.y += entity.velocity.y;
-    }
+      if (this.waypointReached())
+      {
+        if(!this.nextWaypoint())
+        {
+          return this.exit();
+        };
+      }
+      else
+      {
+        this.x += entity.velocity.x;
+        this.y += entity.velocity.y;
+      };
+    };
+
+    entity.waypointReached = function()
+    {
+      return (
+            (this.velocity.x > 0 && this.x >= this.waypoint.x)
+        ||  (this.velocity.x < 0 && this.x <= this.waypoint.x)
+        ||  (this.velocity.y > 0 && this.y >= this.waypoint.y)
+        ||  (this.velocity.y < 0 && this.y <= this.waypoint.y)
+      );
+    };
+
+    entity.nextWaypoint = function()
+    {
+      if(!this.waypoint.x)
+      {
+        this.waypoint = Object.assign({}, game.map.waypoints[this.waypointIndex]);
+        this.x = this.waypoint.x;
+        this.y = this.waypoint.y;
+      };
+
+      var oldX = this.waypoint.x;
+      var oldY = this.waypoint.y;
+
+      this.waypointIndex++;
+      if(!game.map.waypoints[this.waypointIndex]) return false;
+      this.waypoint = Object.assign({}, game.map.waypoints[this.waypointIndex]);
+
+      if(oldX == this.waypoint.x)
+        this.velocity.x = 0;
+      else
+        this.velocity.x = (this.x < this.waypoint.x ? entity.speed : -entity.speed);
+
+      if(oldY == this.waypoint.y)
+        this.velocity.y = 0;
+      else
+        this.velocity.y = (this.y < this.waypoint.y ? entity.speed : -entity.speed);
+
+      return true;
+    };
+
+    entity.exit = function()
+    {
+
+    };
+
+    entity.nextWaypoint();
+
+    return entity;
   }
 };
 
